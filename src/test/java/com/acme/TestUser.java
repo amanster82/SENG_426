9beab4b95c03f5785b580fc058d5ff78a5afdea1
Justin Richard <justinr@uvic.ca>
2017-06-05 16:44:57 -0700

    refactoring test options

--- src/test/java/com/acme/TestUser.java
+++ src/test/java/com/acme/TestUser.java
@@ -1,5 +1,8 @@
 package com.acme;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -7,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestUser {
@@ -20,74 +24,20 @@ public class TestUser {
 		
 		// Set up the driver global to this class
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
 	}
-
-	@Test
-	public void SignInValid() {
-		
-		driver.get("http://localhost:8080/");
-		
-		driver.findElement(By.linkText("Register")).click();
-		driver.findElement(By.id("email")).sendKeys("garbarge@garbage.com");
-		driver.findElement(By.id("password")).sendKeys("testpass");
-		driver.findElement(By.id("confirmPassword")).sendKeys("testpass");
-		driver.findElement(By.cssSelector(".btn")).click();
-	}
-	
+ 
 	// Attempt to sign in with valid info and succeed
 	@Test
 	public void SignInValid() {
-		
-	}
-	
-	// Click the sign out button, ensure no longer signed in
-	@Test
-	public void SignOut() {
-		
-	}
-	
-	// Attempt to sign in with wrong info, should get error message
-	@Test
-	public void SignInInvalid() {
-		
-	}
-	
-	// After a failed sign in, click on forgot password and fill form check for success message
-	@Test
-	public void ResetPasswordValid() {
-		
-	}
-	
-	// Attempt a password reset with invalid email address and check for error message
-	@Test
-	public void ResetPasswordInvalid() {
-		
-	}
-	
-	// Change first/last/email on record and check for success message
-	@Test
-	public void AccountSettingsValid() {
-		// Need to log user back in first!
-		
-	}
-	
-	// Change first/last/email to invalid data and check for fail message
-	@Test
-	public void AccountSettingsInvalid() {
-		
-	}
-	
-	// Change password and check for success
-	@Test
-	public void PasswordChangeValid() {
-		
-	}
-	
-	// Enter mismatching passwords and check error
-	@Test
-	public void PasswordChangeInvalid() {
-		
+		driver.get("http://localhost:8080/#/");
+		driver.findElement(By.linkText("Sign in")).click();
+		driver.findElement(By.id("username")).sendKeys("frank.paul@acme.com");
+		driver.findElement(By.id("password")).sendKeys("starwars");
+		driver.findElement(By.cssSelector(".btn")).click();
+		assertTrue(driver.findElement(By.id("account-menu")).isDisplayed());
 	}
 	
 	// AcmePass app should load list of saved passwords
@@ -126,7 +76,7 @@ public class TestUser {
 		
 	}
 
-	// Generate a random password and ensure output matches paramters given
+	// Generate a random password and ensure output matches parameters given
 	@Test
 	public void UsePasswordGenerator() {
 		
@@ -137,6 +87,7 @@ public class TestUser {
 	public void DeleteSavedPassword() {
 		
 	}
+	
 	
 	@After
 	public void tearDown() throws Exception {
